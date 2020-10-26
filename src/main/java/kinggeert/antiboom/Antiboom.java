8e22a8a382b6c4e8feeb99ff6a506f813d8bff74
kinggeert <geertbruls@outlook.com>
2020-10-27 00:36:52 +0100

    added functionality

--- src/main/java/kinggeert/antiboom/Antiboom.java
+++ src/main/java/kinggeert/antiboom/Antiboom.java
@@ -1,17 +1,28 @@
 package kinggeert.antiboom;
 
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Antiboom extends JavaPlugin {
+public final class Antiboom extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.getServer().getPluginManager().registerEvents(this, this);
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onExplode(EntityExplodeEvent event) {
+        if (event.getEntityType() == EntityType.CREEPER || event.getEntityType() == EntityType.FIREBALL) {
+            event.blockList().clear();
+        }
     }
 }
